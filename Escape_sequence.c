ea752a913a269e40e2dc83b419c6d21edaf6bc3b
manodhiambo <manodhiambo@gmail.com>
2021-11-06 16:00:07 +0300

    A program on escape sequences

---
+++ Escape_sequence.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * A program to understand escape sequence
+ * Drawing a tree on console
+ * @\a - Alarm or Beep
+ * @\b - Backspace
+ * @\f - Form Feed
+ * @\n - New Line
+ * @\t - Tab (Horizotal)
+ * @\v - Vertical Tab
+ * @\\ - Backslash
+ * @\' - Single Quote
+ * @\" - Double Quote
+ * @\? - Question Mark
+ * @\nnn - Octal Number
+ * @\xhh - hexadecimal number
+ * @\0 - Null
+ * Return: Nothing
+ */
+
+main()
+{
+	printf("\n");
+	printf("   /|\        \n");
+	printf("  / | \       \n");
+	printf(" /  |  \      \n");
+	printf("/   |   \     \n");
+	printf("_________\    \n");
+	printf("  || ||       \n");
+	printf("  || ||       \n");
+	return 0;
+}
